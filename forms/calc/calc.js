f6f5bf9842b7219fa631c5b2f174b3bc573fcdef
shaiannesunagawa <sys74530@creighton.edu>
2021-03-05 19:11:06 -0600

    working code

--- forms/calc/calc.js
+++ forms/calc/calc.js
@@ -1,3 +1,4 @@
+/*
 function calculate (num1, num2) {
     let sum = num1 + num2
     return sum
@@ -5,5 +6,8 @@ function calculate (num1, num2) {
 
 let number1 = Number(prompt('Enter a number'))
 let number2 = Number(prompt('Enter a second number'))
+
 let summedNumbers = calculate(number1, number2)
+
 console.log(`The sum of ${number1} and ${number2} is ${summedNumbers}.`)
+*/
--- forms/while/while.js
+++ forms/while/while.js
@@ -2,5 +2,6 @@
 let i = 1
 while (i <= 10) {
     console.log(`This is loop iteration ${i}`)
+    i++
     }
-    */
+*/
